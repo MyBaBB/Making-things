6d5761ae3e5f2c7fd94bbd50881655ce58950063
Brett Baker <GitBaBB@MyBaBB.com>
2023-11-13 09:05:23 -0800

    ok I loaded mycustom  Clean-and-Sweet file to it and now to test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Sidenav from './components/Sidenav'
+function App() {
+
+  return (
+<div className='top-2'>
+   
+   <Sidenav />
+</div>
+
+  )
+}
+
+export default App
