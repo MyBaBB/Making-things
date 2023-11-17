519839b5db821cfe00214a9c2cc83b91ae43bc88
Brett Baker <GitBaBB@MyBaBB.com>
2023-11-17 08:48:30 -0800

    ok I learned how to install google fonts.  Nice,  Here is the example for Creepster in TestFonts component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import { useState } from 'react'
-import Sidenav from './components/Sidenav'
-import CountdownTimer from './components/CountdownTimer'
-import Frank from './components/Frank'
+ 
+import TestFonts from './components/TestFonts'
+ 
 function App() {
 
   return (
-    <div className='BodyWrapper flex justify-center'>
-      <div className='block top-2 border-2 border-red-500 w-3/12'>
-        <Sidenav />
-      </div>
-      <div className='block top-2 border-2 border-green-400 w-3/12'>
-        <CountdownTimer />
-      </div>
+     
       <div>
-        <Frank />
+        < TestFonts/>
       </div>
-    </div>
+       
+       
+    
   )
 }
 
---
+++ src/components/TestFonts.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React
+ from 'react'
+
+const TestFonts = () => {
+  return (
+    <div className='Creepster'>
+      <p>Testing google fonts</p>
+    </div>
+  )
+}
+
+export default TestFonts
