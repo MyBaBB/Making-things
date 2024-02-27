a886dc625930fe5616ff653744a44ac4428cf27c
Brett Baker <GitBaBB@MyBaBB.com>
2024-02-27 04:59:16 -0800

    testing a component for a discord help

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 import React from 'react'
 // import Accordion from './components/Accordion'
 // import CountdownTimer from './components/CountdownTimer'
-import Carousel from './components/Carousel'
+// import Carousel from './components/Carousel'
+import TestComponent from './TestFolder/TestComponent'
 const App = () => {
   return (
     <div className='bg-emerald-900 w-screen h-screen'>
      {/* <Accordion /> */}
      {/* <CountdownTimer /> */}
-     <Carousel />
+     <TestComponent />
     
          </div>
   )
---
+++ src/TestFolder/TestComponent.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const TestComponent = () => {
+  return (
+    <div>
+        <li>
+  <input 
+    type="checkbox"
+    className="m-2 rounded-md focus:outline-none w-4 h-4 border-grey-400 border-solid border-2 appearance-none bg-orange-500  checked:bg-blue-500"
+   
+  />
+
+</li>
+        
+        TestComponent
+        
+        </div>
+  )
+}
+
+export default TestComponent
